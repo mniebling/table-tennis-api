d1d9693db1a940ef688130c549c9140eaf3ec5a4
Mike Niebling <michael.niebling@gmail.com>
2016-10-27 21:31:48 -0500

    Update provision task to create api_keys table
    This was tricky; my first thought was to change `createTable` to accept
    a parameter of the table name and chain a few `.then`s invoking this
    new function.
    
    However, a construct like `.then(createTable('players')` was kicking
    off the create promise immediately, so the chain became asynchronous
    and the connection wasn't getting passed between handlers correctly.
    
    The solution was to use partial application to create functions
    specific to each parameter. This way the promises go back to executing
    one at a time because they aren't *invoked* immediately.

--- tasks/provision.js
+++ tasks/provision.js
@@ -1,4 +1,5 @@
 // Run this script to set up the database for the first time.
+const _ = require('lodash')
 const rethink = require('rethinkdb')
 
 
@@ -17,7 +18,15 @@ rethink
   .bind({})
   .then(bindConnection)
   .then(createDatabase)
-  .then(createTables)
+
+  // If we just call the functions with the table names we want, as arguments
+  // to `then`, the chain becomes asynchronous. Instead, we use partial
+  // application to create some functions with hard-coded arguments. These
+  // functions will get called in synchronous order.
+  .then(_.partial(createTable, 'players'))
+  .then(_.partial(createTable, 'api_keys'))
+
+  .then(closeConnection)
   .catch(console.error)
   .finally(process.exit)
 
@@ -25,6 +34,12 @@ rethink
 // the chain's `this` scope, which is created by `bind({})`.
 function bindConnection (connection) {
   this.connection = connection
+  return
+}
+
+// Close the DB connection that we have been passing through the chain's scope.
+function closeConnection () {
+  this.connection.close()
   return
 }
 
@@ -38,7 +53,7 @@ function createDatabase () {
     .then(dbExists => {
 
       if (dbExists) {
-        console.log('The `tabletennis` database already exists.')
+        console.log('The "tabletennis" database already exists.')
         return
       }
 
@@ -52,24 +67,24 @@ function createDatabase () {
 }
 
 // Create the necessary tables if they do not already exist.
-function createTables () {
+function createTable (tableName) {
 
   return rethink
     .db('tabletennis')
     .tableList()
-    .contains('players')
+    .contains(tableName)
     .run(this.connection)
-    .then(tablesExist => {
+    .then(tableExists => {
 
-      if (tablesExist) {
-        console.log('The `players` table already exists.')
+      if (tableExists) {
+        console.log(`The "${tableName}" table already exists.`)
         return
       }
 
       else {
         return rethink
           .db('tabletennis')
-          .tableCreate('players')
+          .tableCreate(tableName)
           .run(this.connection)
           .then(console.log) // Todo: wrap JSON.stringify
       }
