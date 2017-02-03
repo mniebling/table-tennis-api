767e1fc9698729dbe15fe659be5d334088cb0550
Mike Niebling <michael.niebling@gmail.com>
2017-02-03 16:53:36 -0600

    Return player objects instead of ids in get singles match (#6)
    This way the get singles match call has more useful response and
    clients can display relevant information without making additional
    calls themselves.

--- endpoints/singles/get.js
+++ endpoints/singles/get.js
@@ -1,4 +1,5 @@
 // GET '/v1/singles/:id'
+const _ = require('lodash')
 const rethink = require('rethinkdb')
 const validate = require('./services/find.validate')
 const map = require('./services/get.map')
@@ -20,14 +21,44 @@ function getSinglesMatch (request, response) {
     .table('singles_matches')
     .get(request.params.id)
     .run(request._dbConnection)
+    .bind({})
+
+    // Build the Match object
     .then(dbResult => {
 
-      var output = map.result(dbResult)
+      this.match = dbResult
+
+      var findWinner = rethink
+          .db('tabletennis')
+          .table('players')
+          .get(dbResult.winner)
+          .run(request._dbConnection)
+          .then(winnerResult => {
+            this.match.winner = winnerResult
+          })
+
+      var findLoser = rethink
+          .db('tabletennis')
+          .table('players')
+          .get(dbResult.loser)
+          .run(request._dbConnection)
+          .then(loserResult => {
+            this.match.loser = loserResult
+          })
+
+      return Promise.all([ findWinner, findLoser ])
+    })
+
+    // Map the match object and respond to the request.
+    .then(() => {
+
+      var output = map.result(this.match)
 
       return response
         .status(output.code)
         .json(output.body)
     })
+
     .catch(console.error)
 }
 
